965e951a1b180e4d1bac120d87c53ffe7bfd7390
VJ1309 <varunjain1309@gmail.com>
2023-01-09 13:04:53 +0530

    Update app1.py

--- app1.py
+++ app1.py
@@ -7,30 +7,37 @@ st.set_page_config(page_title='Collector Efficiency')
 st.title("Collector's Efficiency 📈")
 st.subheader('Feed me with your Excel file')
 
+st.sidebar.header('User Input Features')
+#selected_year = st.sidebar.selectbox('Year', list(reversed(range(1950,2020))))
+
 uploaded_file = st.file_uploader("Choose a file")
-if uploaded_file:
-    st.markdown('---')
+if uploaded_file is not None:
     df = pd.read_csv(uploaded_file)
-    st.dataframe(df)
+
+    #st.dataframe(df)
     df['CompanyCode'] = df['CompanyCode'].astype(str)
-    groupby_column = st.selectbox(
+    df['Action Assigned Date'] = pd.to_datetime(df['Action Assigned Date'])
+selected_year = st.sidebar.selectbox('Year', list(reversed(df['Action Assigned Date'].dt.year.unique())))
+
+groupby_column = st.sidebar.selectbox(
         'What would you like to analyse?',
         ('CompanyCode', 'Collector Name', 'Customer Name', 'Action Assigned Date'),
     )
 
      # -- GROUP DATAFRAME
-    output_columns = ['Amount']
-    df_grouped = df.groupby(by=[groupby_column], as_index=False)[output_columns].sum()
-
+output_columns = ['Amount']
+df_grouped = df.groupby(by=[groupby_column], as_index=False)[output_columns].sum()
+   
     # -- PLOT DATAFRAME
-    fig = px.bar(
+
+fig = px.bar(
         df_grouped,
         x=groupby_column,
         y='Amount',
         template='plotly_white',
         title=f'<b>Amount by {groupby_column}</b>'
-    )
-    st.plotly_chart(fig)
+        )
+st.plotly_chart(fig)
 
 
 #uploaded_file = st.file_uploader("Choose a file")
@@ -42,13 +49,6 @@ if uploaded_file:
 
 #O2C = pd.read_csv("D:\Python\Datasets\O2C.csv")
 
-
-
-#st.sidebar.title("Functions")
-
-#choice = st.sidebar.selectbox("Select Functional Area",function)
-#if choice == "O2C":
-#		st.write(O2C)
 
 #species_option = st.selectbox('Select Columns',('sepal_length','sepal_width','petal_length','petal_width','species'))
 #data = explore_data(my_dataset)
