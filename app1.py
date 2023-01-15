71a2842c8b27f1eb15cd871409ce84c568a880fe
VJ1309 <varunjain1309@gmail.com>
2023-01-15 19:42:14 +0530

    Update app1.py

--- app1.py
+++ app1.py
@@ -1,73 +1,98 @@
 import pandas as pd
 import streamlit as st
 import plotly.express as px
+from streamlit_option_menu import option_menu
+
+
+
 
 st.markdown(
     """
     <style>
     .main {
-    background-color: #F5F5F5;
+    background-color: #eaeae1;
     }
     </style>
     """,
     unsafe_allow_html=True
 )
-st.set_page_config(page_title='Collector Efficiency')
-st.title("Collector's Efficiency 📈")
-st.subheader('Feed me with your Excel file')
 
-st.sidebar.header('User Input Features')
-#selected_year = st.sidebar.selectbox('Year', list(reversed(range(1950,2020))))
 
-uploaded_file = st.file_uploader("Choose a file")
-if uploaded_file is not None:
-    df = pd.read_csv(uploaded_file)
+st.title('Collector Efficiency')
+#st.text('In this project I look into the transactions of taxis in NYC. ...')
+selected = option_menu(
+            menu_title=None,  # required
+            options=["Home", "Projects", "Contact"],  # required
+            icons=["house", "book", "envelope"],  # optional
+            menu_icon="cast",  # optional
+            default_index=0,  # optional
+            orientation="horizontal",
+            styles={
+                "container": {"padding": "0!important", "background-color": "#a8b5b4"},
+                "icon": {"color": "orange", "font-size": "25px"},
+                "nav-link": {
+                    "font-size": "25px",
+                    "text-align": "left",
+                    "margin": "0px",
+                    "--hover-color": "#eee",
+                },
+                "nav-link-selected": {"background-color": "#2358eb"},
+            },
+        )
 
-    #st.dataframe(df)
-    df['CompanyCode'] = df['CompanyCode'].astype(str)
-    df['Action Assigned Date'] = pd.to_datetime(df['Action Assigned Date'])
-selected_year = st.sidebar.selectbox('Year', list(reversed(df['Action Assigned Date'].dt.year.unique())))
+        
+#st.set_page_config(page_title='Collector Efficiency')
+#st.title("Collector's Efficiency 📈")
+#st.subheader('Feed me with your Excel file')
 
-groupby_column = st.sidebar.selectbox(
+
+#selected_year = st.sidebar.selectbox('Year', ('2020','2021','2022'))
+
+if selected == 'Home':
+    st.markdown('''
+         #### Percentage of members with goals asdadasdasdasdasda
+        #### asfasfdasfasf
+        ''', unsafe_allow_html=True)
+elif selected == 'Projects':
+    st.sidebar.header('User Input Features')
+    uploaded_file = st.file_uploader("Choose a file")
+    if uploaded_file is not None:
+        df = pd.read_csv(uploaded_file, parse_dates=["Action Assigned Date"])
+        df["Action Assigned Date Year"] = df["Action Assigned Date"].dt.year
+        startdate = ('2020', '2021', '2022')
+        st.write(df.info())
+    
+
+    
+
+    
+    
+    
+    #startdate = ('2022-01-01')
+    
+    
+        df['CompanyCode'] = df['CompanyCode'].astype(str)
+    #df['Action Assigned Date'] = pd.to_datetime(df['Action Assigned Date'])
+    #selected_year = st.sidebar.selectbox('Year', list(reversed(df['Action Assigned Date'].dt.year.unique())))
+
+        groupby_column = st.sidebar.selectbox(
         'What would you like to analyse?',
         ('CompanyCode', 'Collector Name', 'Customer Name', 'Action Assigned Date'),
-    )
+        )
 
      # -- GROUP DATAFRAME
-output_columns = ['Amount']
-df_grouped = df.groupby(by=[groupby_column], as_index=False)[output_columns].sum()
+        output_columns = ['Amount']
+        df_grouped = df.groupby(by=[groupby_column], as_index=False)[output_columns].sum()
    
     # -- PLOT DATAFRAME
 
-fig = px.bar(
+        fig = px.bar(
         df_grouped,
         x=groupby_column,
         y='Amount',
         template='plotly_white',
         title=f'<b>Amount by {groupby_column}</b>'
         )
-st.plotly_chart(fig)
-
-
-uploaded_file = st.file_uploader('Choose a XLSX file', type='xlsx')
-if uploaded_file:
-    st.markdown('---')
-    df = pd.read_excel(uploaded_file, engine='openpyxl')
-    st.dataframe(df)
-
-#uploaded_file = st.file_uploader("Choose a file")
-#if uploaded_file is not None:
-  #df = pd.read_csv(uploaded_file)
- # st.write(df)
-
-#function = ["O2C","Make"]
-
-#O2C = pd.read_csv("D:\Python\Datasets\O2C.csv")
-
-
-#species_option = st.selectbox('Select Columns',('sepal_length','sepal_width','petal_length','petal_width','species'))
-#data = explore_data(my_dataset)
-#if species_option == 'sepal_length':
-#	st.write(data['sepal_length'])
-#elif species_option == 'sepal_width':
-#	st.write(data['sepal_width'])
+        st.plotly_chart(fig)
+elif selected == 'Contact':
+    st.write("Varun")
